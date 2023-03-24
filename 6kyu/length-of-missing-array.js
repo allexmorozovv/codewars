d64c634746eae3289fd4681f69927d4d48c838c6
allexmorozovv <allexmorozovv@gmail.com>
2023-03-24 06:47:59 +0300

    fix length-of-missing-array.js

--- 6kyu/length-of-missing-array.js
+++ 6kyu/length-of-missing-array.js
@@ -13,16 +13,18 @@
 
 
 function getLengthOfMissingArray(arrayOfArrays) {
-    if (!arrayOfArrays || arrayOfArrays.length === 0) {
-        return 0;
-    }
-    arrayOfArrays.sort((a, b) => a.length - b.length);
+    const result = (arrayOfArrays || []).map(el => el ? el.length : 0).sort((a, b) => a - b)
 
-    for (let i = 0; i < arrayOfArrays.length; i++) {
-        if (arrayOfArrays[i].length !== i + 1) {
-            return i + 1
+    if (result.includes(0)) {
+        return 0
+    }
+
+    for (let i = 0; i < result.length - 1; i++) {
+        if (result[i] + 1 !== result[i + 1]) {
+            return result[i] + 1
         }
     }
+    return 0
 }
 
 console.log(getLengthOfMissingArray([[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]]))
